8bf56476689c71fdd7bf18d0bc4642f75ee4781a
0xpr0f <0xprofoz@gmail.com>
2025-02-19 00:57:48 +0100

    dam it and work

--- src/components/pc/drives/Storage&Hooks/PregenInteractions.ts
+++ src/components/pc/drives/Storage&Hooks/PregenInteractions.ts
@@ -80,7 +80,7 @@ export function usePregenTransaction({
             value,
           },
           {
-            timeout: 60000,
+            timeout: 600000,
           }
         )
       } else {
@@ -97,7 +97,7 @@ export function usePregenTransaction({
             value,
           },
           {
-            timeout: 60000,
+            timeout: 600000,
           }
         )
       }
